faa994704dd36afe6bf4244f98fac81ccd018d2f
hak <hak@tutao.de>
2024-11-14 15:15:05 +0100

    create the function that toggles a book’s read status on your Book prototype

--- Library.js
+++ Library.js
@@ -1,34 +1,53 @@
 const myLibrary = [
-	{index: "id-1", title:'The Picture of Dorian Gray', author: 'Oscar Wilde', pages: '304'},
-	{index: "id-2", title:'The Old Man and the Sea', author:'Ernest Hemingway', pages: '46'},
-	{index: "id-3", title:'Blue Ocean Strategy', author:'W. Chan Kim and Renée Mauborgne', pages: '320'},
+	// {index: "id-1", title: 'The Picture of Dorian Gray', author: 'Oscar Wilde', pages: '304'},
+	// {index: "id-2", title: 'The Old Man and the Sea', author: 'Ernest Hemingway', pages: '46'},
+	// {index: "id-3", title: 'Blue Ocean Strategy', author: 'W. Chan Kim and Renée Mauborgne', pages: '320'},
+	new Book('The Picture of Dorian Gray', 'Oscar Wilde', '304'),
+	new Book('The Old Man and the Sea', 'Ernest Hemingway', '46'),
+	new Book('Blue Ocean Strategy', 'W. Chan Kim and Renée Mauborgne', '320'),
 ];
 
-function Book() {
-	// the constructor...
+//Конструктор книги
+function Book(title, author, pages, status = 'This book has not been read.', index) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.status = status;
+	this.index = index;
 }
+
+//Метод для переключения статуса прочтения
+Book.prototype.toggleStatus = function () {
+	this.status = this.status === 'This book has been read.' ? 'This book has not been read.' : 'This book has been read.';
+}
+
 
 function addBookToLibrary(book) {
 
 	//Check if the book already exists in the library
 	const existBook = myLibrary.some(existingBook =>
-	existingBook.title === book.title && existingBook.author === book.author);
+		existingBook.title === book.title && existingBook.author === book.author);
 
-	if(existBook) {
+	if (existBook) {
 		alert('This book already exists!')
 		return;
 	}
 
-	const {title, author, pages, status, additionalInfo, index} = book; //Destructurisation of the book, take the value in one single code.
-	const createdBook = {
-		index: index,
-		title: title,
-		author: author,
-		pages:pages,
-		status: status,
-		additionalInfo: additionalInfo
-	};
-		myLibrary.push(createdBook);
+// 	const {title, author, pages, status, additionalInfo, index} = book; //Destructurisation of the book, take the value in one single code.
+// 	const createdBook = {
+// 		index: index,
+// 		title: title,
+// 		author: author,
+// 		pages: pages,
+// 		status: status,
+// 		additionalInfo: additionalInfo
+// 	};
+// 	myLibrary.push(createdBook);
+// }
+
+	const index = myLibrary.length + 1; // Генерируем индекс (можно менять по необходимости)
+	const createdBook = new Book(book.title, book.author, book.pages, book.status, index);
+	myLibrary.push(createdBook);
 }
 
 
@@ -38,66 +57,70 @@ newBookBtn.setAttribute('style', 'width:80px; height:40px; margin-bottom: 20px;'
 document.body.appendChild(newBookBtn);
 
 
-	function displayBooks(books) {
-		const bookList = document.createElement('div');                                        //<div id='book-list'></div>
-		bookList.setAttribute('id', 'book-list');
-		bookList.setAttribute('style', 'display: flex; flex-direction: row; gap:20px;')
+function displayBooks(books) {
+	const bookList = document.createElement('div');                                        //<div id='book-list'></div>
+	bookList.setAttribute('id', 'book-list');
+	bookList.setAttribute('style', 'display: flex; flex-direction: row; gap:20px;')
 
 
 //!!!!!!!!!!!!!!!!!!!!!
-		while (bookList.firstChild) {
-			bookList.removeChild(bookList.firstChild);
-		}
-
-
-		books.forEach( (book, index) => {
-			const bookItem = document.createElement('div') ;                                    //<div class='book-item'></div>
-			bookItem.classList.add('book-item');
-			bookItem.setAttribute('style', ' border-radius: 25px; padding: 10px; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px; margin-bottom: 10px;');
-
-			// bookItem.innerHTML = `<h2>${book.title}</h2> <p> Author: ${book.author}</p> <p>Pages: ${book.pages}</p>`;
-			const titleElm = document.createElement('h2');                                  //<h2>book.title</h2>
-			titleElm.textContent = book.title;
-			const authorElm = document.createElement('p');                                //<p>book.author</p>
-			authorElm.textContent = book.author;
-			const pagesElm = document.createElement('p');                                 //<p>book.pages</p>
-			pagesElm.textContent = `${book.pages} pages`;
-
-			const removeBookBtn = document.createElement('button');
-			removeBookBtn.textContent = 'Remove Book';
-
-
-
-			//Remove buttom
-			removeBookBtn.onclick = () => {
-				myLibrary.splice(index, 1); // Удаляем книгу из массива
-				 displayBooks(myLibrary); // Обновляем отображение книг
-			};
-
-
-			const statusChangeBtn = document.createElement('button');
-			statusChangeBtn.textContent = 'Change status';
-
-				bookItem.appendChild(titleElm);
-			bookItem.appendChild(authorElm);
-			bookItem.appendChild(pagesElm);
-			bookItem.appendChild(removeBookBtn);
-			bookItem.appendChild(statusChangeBtn);
-
-			bookList.appendChild(bookItem);
-		});
-
-//Если списка с такими книгами нет на странице, то добавить, а если есть то мы заменяем его новым списком.
-		if (!document.getElementById('book-list')) {
-			document.body.appendChild(bookList);
-		} else {
-			document.body.replaceChild(bookList, document.getElementById('book-list'));
-		}
-
+	while (bookList.firstChild) {
+		bookList.removeChild(bookList.firstChild);
 	}
 
-displayBooks(myLibrary);
 
+	books.forEach((book, index) => {
+		const bookItem = document.createElement('div');                                    //<div class='book-item'></div>
+		bookItem.classList.add('book-item');
+		bookItem.setAttribute('style', ' border-radius: 25px; padding: 10px; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px; margin-bottom: 10px;');
+
+		// bookItem.innerHTML = `<h2>${book.title}</h2> <p> Author: ${book.author}</p> <p>Pages: ${book.pages}</p>`;
+		const titleElm = document.createElement('h2');                                  //<h2>book.title</h2>
+		titleElm.textContent = book.title;
+		const authorElm = document.createElement('p');                                //<p>book.author</p>
+		authorElm.textContent = book.author;
+		const pagesElm = document.createElement('p');                                 //<p>book.pages</p>
+		pagesElm.textContent = `${book.pages} pages`;
+		const statusElm = document.createElement('p');
+		statusElm.textContent = book.status;
+
+		const removeBookBtn = document.createElement('button');
+		removeBookBtn.textContent = 'Remove Book';
+
+		//Remove buttom
+		removeBookBtn.onclick = () => {
+			myLibrary.splice(index, 1); // Удаляем книгу из массива
+			displayBooks(myLibrary); // Обновляем отображение книг
+		};
+
+
+		const statusChangeBtn = document.createElement('button');
+		statusChangeBtn.textContent = 'Change status';
+		statusChangeBtn.onclick = () => {
+			book.toggleStatus(); // Переключаем статус
+			displayBooks(myLibrary);
+		}
+
+		bookItem.appendChild(titleElm);
+		bookItem.appendChild(authorElm);
+		bookItem.appendChild(pagesElm);
+		bookItem.appendChild(statusElm);
+		bookItem.appendChild(removeBookBtn);
+		bookItem.appendChild(statusChangeBtn);
+
+		bookList.appendChild(bookItem);
+	});
+
+//Если списка с такими книгами нет на странице, то добавить, а если есть то мы заменяем его новым списком.
+	if (!document.getElementById('book-list')) {
+		document.body.appendChild(bookList);
+	} else {
+		document.body.replaceChild(bookList, document.getElementById('book-list'));
+	}
+
+}
+
+displayBooks(myLibrary);
 
 
 const newBookDialog = document.getElementById('newBookDialog');
@@ -111,10 +134,6 @@ document.getElementById('cancelBtn').addEventListener('click', () => {
 });
 
 
-
-
-
-
 //Modal window
 
 // Prevent confirmBtn - the form from being sent
@@ -123,23 +142,23 @@ document.getElementById('confirmBtn').addEventListener('click', (event) => {
 
 
 //// Get values from form fields
- const modalBTitle = document.getElementById('book-title').value;
- const modalBAuthor = document.getElementById('book-author').value || "Not provided";
- const modalBPages = document.getElementById('book-pages').value || "Not provided";
- const modalBStatus = document.querySelector('input[name="book_status"]:checked')?.value || "This book has not been read.";
- const modalBAddInfo = document.getElementById('subject').value;
+	const modalBTitle = document.getElementById('book-title').value;
+	const modalBAuthor = document.getElementById('book-author').value || "Not provided";
+	const modalBPages = document.getElementById('book-pages').value || "Not provided";
+	const modalBStatus = document.querySelector('input[name="book_status"]:checked')?.value || "This book has not been read.";
+	const modalBAddInfo = document.getElementById('subject').value;
 
- const newBook = {
-	 title: modalBTitle,
-	 author: modalBAuthor,
-pages: modalBPages,
-status: modalBStatus,
-	additionalInfo: modalBAddInfo
+	const newBook = {
+		title: modalBTitle,
+		author: modalBAuthor,
+		pages: modalBPages,
+		status: modalBStatus,
+		additionalInfo: modalBAddInfo
 	};
 
 
- addBookToLibrary(newBook);
- displayBooks(myLibrary);
+	addBookToLibrary(newBook);
+	displayBooks(myLibrary);
 
 	newBookDialog.close();
 });
